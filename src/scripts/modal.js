5338c0dcf51a469c92f160b31f7c0d7dea972c51
HalianRC <125613474+HalianRC@users.noreply.github.com>
2023-05-25 15:01:04 -0300

    page-login

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,1 +1,14 @@
 /* Desenvolva seu código aqui... */
+function myModal(){
+  const openModal = document.querySelector(".btn-cadastrar")
+  const modal_container = document.querySelector(".modal__container")
+openModal.addEventListener("click", function(){
+ modal_container.showModal()
+
+})
+
+
+
+}
+myModal()
+
